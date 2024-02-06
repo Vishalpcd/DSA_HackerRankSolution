e143b17b2141200499e1f1a133ff4ff11c3c1f84
vishalpcd <vpathak154@gmailc.com>
2024-02-06 23:48:36 +0530

    Day 1 - Added 2d array solution

--- HackerRankPractice/Program.cs
+++ HackerRankPractice/Program.cs
@@ -1,8 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-
+﻿
 List<List<int>> arr = new List<List<int>>();
 arr.Add(new List<int>() { 1, 2, 3, 4, 5, 6 });
 arr.Add(new List<int>() { 1, 2, 3, 4, 5, 6 });
@@ -11,4 +7,4 @@ arr.Add(new List<int>() { 1, 2, 3, 4, 5, 6 });
 arr.Add(new List<int>() { 1, 2, 3, 4, 5, 6 });
 arr.Add(new List<int>() { 1, 2, 3, 4, 5, 6 });
 
-Console.WriteLine(arr[0][1]); 
+TwoD_Array_DS_Day1.GetMaximumHourGlassSum(arr);
---
+++ HackerRankPractice/Solutions/2D_Array_DS_Day1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class TwoD_Array_DS_Day1
+{
+    //2d array in Data structure see the detailed problem statement from the given link https://www.hackerrank.com/challenges/2d-array/problem?isFullScreen=true
+    public static void GetMaximumHourGlassSum(List<List<int>> arr)
+    {
+        var rowLength = arr.Count;
+        var columnLength = arr[0].Count;
+        List<int> sumValues = new List<int>();
+        for(int i = 0; i < columnLength-2; i++)
+        {
+            for(int j = 0; j < rowLength-2; j++)
+            {
+                var sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+                sumValues.Add(sum);
+            }
+
+        }
+        Console.WriteLine(sumValues.Max());
+    }
+}
