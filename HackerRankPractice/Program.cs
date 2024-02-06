67079e6fe96e5a61fb576a33a2f57ee7a7966d8f
vishalpcd <vpathak154@gmailc.com>
2024-02-06 22:49:30 +0530

    Add project files.

---
+++ HackerRankPractice/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+List<List<int>> arr = new List<List<int>>();
+arr.Add(new List<int>() { 1, 2, 3, 4, 5, 6 });
+arr.Add(new List<int>() { 1, 2, 3, 4, 5, 6 });
+arr.Add(new List<int>() { 1, 2, 3, 4, 5, 6 });
+arr.Add(new List<int>() { 1, 2, 3, 4, 5, 6 });
+arr.Add(new List<int>() { 1, 2, 3, 4, 5, 6 });
+arr.Add(new List<int>() { 1, 2, 3, 4, 5, 6 });
+
+Console.WriteLine(arr[0][1]); 
