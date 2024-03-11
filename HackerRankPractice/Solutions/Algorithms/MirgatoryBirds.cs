f34c94c57f42aaf1e0a203fb94a0d08ddbcad894
vishalpcd <vpathak154@gmailc.com>
2024-03-11 23:27:22 +0530

    Day 1 - Resolved migratory birds problem

---
+++ HackerRankPractice/Solutions/Algorithms/MirgatoryBirds.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class MirgatoryBirds
+{
+    /// <summary>
+    /// Given an array of bird sightings where every element represents a bird type id,
+    /// determine the id of the most frequently sighted type. If more than 1 type has 
+    /// been spotted that maximum amount, return the smallest of their ids.
+    /// https://hackerrank.com/challenges/migratory-birds/problem
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <returns></returns>
+    public static int migratoryBirds(List<int> arr)
+    {
+        var grouped = arr.GroupBy(e => e, e => e)
+            .OrderByDescending(g => g.Count())
+           .ThenBy(g => g.Key);
+
+        return grouped.FirstOrDefault().Key;
+    }
+}
